db34c808efe39728fe78cfecdaf1a76e5c5d1f53
MinSoo Kim <minsoo8449@gmail.com>
2015-12-23 04:06:44 -0800

    Initial Commit

---
+++ CobaltiOSSDK/CobaltiOSSDK.h
@@ -0,0 +1,19 @@
+//
+//  CobaltiOSSDK.h
+//  CobaltiOSSDK
+//
+//  Created by Minsoo Kim on 2015. 12. 23..
+//  Copyright © 2015년 CstLex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CobaltiOSSDK.
+FOUNDATION_EXPORT double CobaltiOSSDKVersionNumber;
+
+//! Project version string for CobaltiOSSDK.
+FOUNDATION_EXPORT const unsigned char CobaltiOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CobaltiOSSDK/PublicHeader.h>
+
+
